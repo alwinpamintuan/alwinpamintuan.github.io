47c369bb0e144d2e051609a9101fbf5d11f91d69
John Alwin Pamintuan <jrpamintuan1@up.edu.ph>
2022-08-22 16:46:53 +0800

    UI changes

--- src/modules/layout/components/header/Header.tsx
+++ src/modules/layout/components/header/Header.tsx
@@ -1,64 +1,74 @@
-import React, { useEffect, useState } from 'react';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { classes } from '@utils/index';
-import styles from './Header.module.css';
-import others from '@components/placeholder/Placeholder.module.css';
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import { classes } from "@utils/index";
+import styles from "./Header.module.css";
+import others from "@components/placeholder/Placeholder.module.css";
 
-type Props = {}
+type Props = {};
 
 export function Header({}: Props) {
   const [openBurger, setOpenBurger] = useState(false);
-  const router = useRouter();
+
+  const links = [
+    "About",
+    "Education",
+    "Work Experience",
+    "Affiliations",
+    "Projects",
+    "Contact",
+  ];
+
+  const handleLinkClick = (link: string) => {
+    document.getElementById(link)?.scrollIntoView(true);
+  };
 
   useEffect(() => {
-    const animationEls = document.querySelectorAll(`.${others.rotating}, .${others.bobbing}`);
-    if(openBurger){
+    const animationEls = document.querySelectorAll(
+      `.${others.rotating}, .${others.bobbing}`
+    );
+    if (openBurger) {
       animationEls?.forEach((el) => {
-        el.classList.add('noanimation');
-      })
-    }else{
+        el.classList.add("noanimation");
+      });
+    } else {
       animationEls?.forEach((el) => {
-        el.classList.remove('noanimation');
-      })
+        el.classList.remove("noanimation");
+      });
     }
-  }, [openBurger])
+  }, [openBurger]);
 
   return (
     <header className={styles.header}>
-      <Link href='/'>
+      <Link href="/">
         <a className={styles.brand}>
-          <span className={styles.alwin}>alwin</span>
-          <span className={styles.pamintuan}>pamintuan</span>
+          <span className={styles.alwin}>Alwin </span>
+          <span className={styles.pamintuan}>Pamintuan</span>
         </a>
       </Link>
 
       <nav className={styles.menu} data-burger={openBurger}>
-        <Link href='#about'>
-          <a className={styles.link}>About</a>
-        </Link>
-        
-        <Link href='#projects'>
-          <a className={styles.link}>Projects</a>
-        </Link>
-
-        <Link href='#contact'>
-          <a className={styles.link}>Contact</a>
-        </Link>
+        {links.map((link) => (
+          <a className={styles.link} onClick={() => handleLinkClick(link)}>
+            {link}
+          </a>
+        ))}
       </nav>
-      
+
       <label className={styles.burgerIcon} data-burger={openBurger}>
-        <input type="checkbox" className={styles.checkbox} onChange={(e) => {
-          setOpenBurger(e.target.checked);
-        }}/>
+        <input
+          type="checkbox"
+          className={styles.checkbox}
+          onChange={(e) => {
+            setOpenBurger(e.target.checked);
+          }}
+        />
 
         <div className={styles.burgerLines}>
-          <span className={classes(styles.line, styles.line1)}/>
-          <span className={classes(styles.line, styles.line2)}/>
-          <span className={classes(styles.line, styles.line3)}/>
+          <span className={classes(styles.line, styles.line1)} />
+          <span className={classes(styles.line, styles.line2)} />
+          <span className={classes(styles.line, styles.line3)} />
         </div>
       </label>
-
     </header>
-  )
+  );
 }
