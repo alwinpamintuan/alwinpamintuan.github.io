f2a95d66df2259ef98f09d0d3c433fa21da77061
alwinpamintuan <48505618+alwinpamintuan@users.noreply.github.com>
2022-08-22 10:02:16 +0000

    Deploying to gh-pages from @ alwinpamintuan/alwinpamintuan.github.io@347d43a1a49a1c2da698407ce1d37f73e7981dd9 🚀

---
+++ _next/static/bcEWn7t-3XPfwvRb2LoDT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/51d3970aa94903c8.css","static/chunks/pages/index-74cd4c3bceccfb39.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
--- _next/static/f7VRkYx0uqOFcncv1OCF6/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/13db9079f90dc7fa.css","static/chunks/pages/index-74cd4c3bceccfb39.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
