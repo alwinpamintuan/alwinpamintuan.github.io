bc3d31f3f72a593f8bb13f7540ff247709516013
John Alwin Pamintuan <jrpamintuan1@up.edu.ph>
2022-09-24 00:31:48 +0800

    Fix unmerged files

--- src/modules/about/About.tsx
+++ src/modules/about/About.tsx
@@ -7,20 +7,12 @@ type Props = {};
 
 export function About({}: Props) {
   return (
-<<<<<<< HEAD
-    <Section>
-=======
     <Section id={`AboutDiv`} justify="center">
->>>>>>> ui-dev
       <Tilt
         options={{
           speed: 10,
         }}
         className={styles.card}
-<<<<<<< HEAD
-        id={`AboutDiv`}
-=======
->>>>>>> ui-dev
       >
         <h4>Hello, I am</h4>
         <h1 className={styles.popout}>John Alwin Pamintuan</h1>
