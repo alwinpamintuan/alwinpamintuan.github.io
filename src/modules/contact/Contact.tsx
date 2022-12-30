35dd0285e625502c004356f2e46e163ea6062340
John Alwin Pamintuan <48505618+alwinpamintuan@users.noreply.github.com>
2022-12-30 23:49:22 +0800

    Animations (#3)
    * Update header
    
    * Change menu style
    
    * UI changes
    
    * Remove deprecated menu functions
    
    * Update section layout
    
    * Update header style
    
    * Add contact section
    
    * Active link update on scroll
    
    * Temporary light mode
    
    * Change light mode colors
    
    * Add hero animations
    
    * Fix unescaped apostrophe
    
    * Increase font size
    
    * Add signature animations
    
    * Import and apply aos
    
    * Install @types/aos
    
    * Animate contact section
    
    * Import and apply aos
    
    * Install @types/aos
    
    * Animate contact section

--- src/modules/contact/Contact.tsx
+++ src/modules/contact/Contact.tsx
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Section, Placeholder } from "@components/index";
 import styles from "./Contact.module.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { aosConfig } from "@utils/index";
 
 type Props = {};
 
 export function Contact({}: Props) {
+  useEffect(() => {
+    AOS.init(aosConfig);
+  }, []);
+
   return (
     <Section id={`ContactDiv`} header={"How can you reach me?"}>
       <div className={`${styles.container}`}>
-        <h3>You can reach me through the following channels:</h3>
+        <h3 data-aos="fade-up">
+          You can reach me through the following channels:
+        </h3>
         <ul className={styles.contacts}>
-          <li>
+          <li data-aos="fade-up" data-aos-delay="250">
             <a
               href="mailto: alwinpamintuan@gmail.com"
               target="_blank"
@@ -21,8 +30,8 @@ export function Contact({}: Props) {
           </li>
         </ul>
 
-        <p className={styles.note}>
-          That&apos;s it for now but I might add more soon!
+        <p className={styles.note} data-aos="fade-left" data-aos-delay="500">
+          That&apos;s it for now but I hope to add more soon!
         </p>
       </div>
     </Section>
