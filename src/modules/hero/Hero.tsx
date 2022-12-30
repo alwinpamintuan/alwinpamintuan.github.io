99ddc5f5d8da2f968badb2fabd2ff88007648c12
alwinpamintuan <alwinpamintuan@gmail.com>
2022-12-30 18:38:23 +0800

    Add hero animations

--- src/modules/hero/Hero.tsx
+++ src/modules/hero/Hero.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { Socials, Tilt, Section } from "@components/index";
 import styles from "./Hero.module.css";
-import { classes } from "@utils/index";
 
 type Props = {};
 
 export function Hero({}: Props) {
   return (
     <Section id={`HeroDiv`} justify="center">
-      <h4>Hello, I am</h4>
+      <h4>
+        <span className={styles.hello}>Hello! &nbsp;</span>
+        <span className={styles.iam}>I am</span>
+      </h4>
       <h1 className={styles.emphasis}>John Alwin Pamintuan</h1>
-      <p>Data Engineer and Frontend Developer 🗃️👩‍💻</p>
+      <p className={styles.subtitle}>
+        Data Engineer and Frontend Developer 🗃️👩‍💻
+      </p>
       <Socials />
     </Section>
   );
