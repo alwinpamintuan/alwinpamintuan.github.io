804039d673028c6ba02073c42c093ca95a113e6e
alwinpamintuan <alwinpamintuan@gmail.com>
2022-12-30 15:38:47 +0800

    Update header style

--- src/modules/experience/Experience.tsx
+++ src/modules/experience/Experience.tsx
@@ -1,14 +1,13 @@
 import React from "react";
-import { Section } from "@components/index";
+import { Placeholder, Section } from "@components/index";
 import styles from "./Experience.module.css";
 
 type Props = {};
 
 export function Experience({}: Props) {
   return (
-    <Section
-      id={`ExperienceDiv`}
-      header={"Where have I been involved?"}
-    ></Section>
+    <Section id={`ExperienceDiv`} header={"Where have I been involved?"}>
+      <Placeholder />
+    </Section>
   );
 }
--- src/modules/projects/Projects.tsx
+++ src/modules/projects/Projects.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { Section } from "@components/index";
+import { Placeholder, Section } from "@components/index";
 import styles from "./Projects.module.css";
 
 type Props = {};
 
 export function Projects({}: Props) {
   return (
-    <Section id={`ProjectsDiv`} header={"What have I worked on?"}></Section>
+    <Section id={`ProjectsDiv`} header={"What have I worked on?"}>
+      <Placeholder />
+    </Section>
   );
 }
