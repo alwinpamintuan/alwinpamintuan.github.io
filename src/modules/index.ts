d157426fa9c8c78d00db811a6d8774dba9a7b3ed
Alwin Pamintuan <alwinpamintuan@gmail.com>
2023-04-22 00:36:52 +0800

    Merge experience and work sections

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,6 +1,5 @@
 export { Layout } from "./layout/Layout";
 export { About } from "./about/About";
 export { Work } from "./work/Work";
-export { Projects } from "./projects/Projects";
 export { Contact } from "./contact/Contact";
 export { Hero } from "./hero/Hero";
