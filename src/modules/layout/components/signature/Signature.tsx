3736efa1add5ed26b1ee5bb5580c5b8076adab56
Alwin Pamintuan <alwinpamintuan@gmail.com>
2023-04-22 21:39:05 +0800

    Link github to signature

--- src/modules/layout/components/signature/Signature.tsx
+++ src/modules/layout/components/signature/Signature.tsx
@@ -1,11 +1,22 @@
 import styles from "./Signature.module.css";
+import contactsJson from "public/data/contacts.json";
 
 type Props = { children?: any };
 
 export function Signature({ children }: Props) {
+  let contacts = contactsJson.contacts;
+  let github = contacts.findIndex((obj) => obj.name.toLowerCase() == "github");
+
   return (
     <div className={styles.signature}>
-      <p className={styles.copyright}>🏃‍♂️💨 🦖</p>
+      <a
+        href={contacts[github].link}
+        className={styles.copyright}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        🏃‍♂️💨 🦖
+      </a>
       {children}
     </div>
   );
