65c9130068cb152d52583547148720a5a6c8df10
Alwin Pamintuan <alwinpamintuan@gmail.com>
2023-04-22 23:16:05 +0800

    Disable moving background on mobile

--- src/modules/layout/components/background/Background.tsx
+++ src/modules/layout/components/background/Background.tsx
@@ -1,13 +1,34 @@
+import { useEffect, useState } from "react";
 import { classes } from "@utils/index";
 import styles from "./Background.module.css";
 
 type Props = {};
 
 export function Background({}: Props) {
+  const [isMobile, setIsMobile] = useState(true);
+
+  useEffect(() => {
+    console.log(window.navigator.userAgent);
+    console.log(/Mobi/i.test(window.navigator.userAgent));
+    setIsMobile(/Mobi/i.test(window.navigator.userAgent));
+  }, []);
+
   return (
     <>
-      <div className={classes(styles.blob, styles.blob1)}></div>
-      <div className={classes(styles.blob, styles.blob2)}></div>
+      <div
+        className={classes(
+          styles.blob,
+          styles.blob1,
+          isMobile ? "" : styles.blob1anim
+        )}
+      ></div>
+      <div
+        className={classes(
+          styles.blob,
+          styles.blob2,
+          isMobile ? "" : styles.blob2anim
+        )}
+      ></div>
     </>
   );
 }
