a1f81a312f06e93417a271e41191b449c79a89d2
John Alwin Pamintuan <48505618+alwinpamintuan@users.noreply.github.com>
2023-04-22 16:42:35 +0800

    Develop (#6)
    * Animated background gradient
    
    * Restructure Layout component folder
    
    * Remove unused Tilt import
    
    * Theme changes
    
    * Adjust blob position
    
    * Streamline sections
    
    * Increase hero section scale
    
    * Layout changes
    
    * Update contacts section
    
    * Update projects, add contacts and experience boilerplate
    
    * Merge experience and work sections
    
    * Update contact and about section
    
    * Remove for now and add projects list
    
    * Update dependencies
    
    * Fix json import issue
    
    * Update dependencies after rebase
    
    * Update dependencies
    
    * Fix json import issue
    
    * Update dependencies after rebase
    
    * Add google ownership metadata and fix next export issue

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,10 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>{"Alwin Pamintuan"}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="google-site-verification"
+          content="lgMHkM85dEnIzUDnCQNCpy4qFBQj3qSe44j-VzV2fPU"
+        />
       </Head>
       <Component {...pageProps} />
     </>
