8f14f53a1816e16c472c6d4c282e82a663106df0
Alwin Pamintuan <alwinpamintuan@gmail.com>
2023-08-14 23:49:56 +0800

    Fix inconsistent vh in mobile

---
+++ src/pages/projects/filipino-fake-news-classifier.tsx
@@ -0,0 +1,14 @@
+import { Layout } from "@modules/index";
+import { Placeholder, Section } from "@components/index";
+
+type Props = {};
+
+function FilipinoFakeNewsClassifier({}: Props) {
+  return (
+    <Section justify={"center"}>
+      <Placeholder />
+    </Section>
+  );
+}
+
+export default FilipinoFakeNewsClassifier;
