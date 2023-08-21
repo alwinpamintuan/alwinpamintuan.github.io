9d98e62c3722c8c5b2b898f30dc0bea5daf37856
Alwin Pamintuan <alwinpamintuan@gmail.com>
2023-08-22 00:09:53 +0800

    Add mouse move effects

--- src/modules/layout/components/background/Background.tsx
+++ src/modules/layout/components/background/Background.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { classes } from "@utils/index";
 import styles from "./Background.module.css";
+import { cursorTo } from "readline";
 
 type Props = {};
 
@@ -10,10 +11,31 @@ export function Background({}: Props) {
   useEffect(() => {
     console.log(window.navigator.userAgent);
     setIsMobile(/Mobi/i.test(window.navigator.userAgent));
+
+    const cursor = document.querySelector(`.${styles.cursor}`) as HTMLElement;
+    const slowBlob = document.querySelector(`.${styles.blob1}`) as HTMLElement;
+    const fastBlob = document.querySelector(`.${styles.blob2}`) as HTMLElement;
+
+    if (cursor && slowBlob && fastBlob) {
+      document.addEventListener("mousemove", (e) => {
+        let x = e.pageX;
+        let y = e.pageY;
+
+        cursor.style.top = `calc(${y}px)`;
+        cursor.style.left = `calc(${x}px)`;
+
+        fastBlob.style.top = `calc(${y}px / 6)`;
+        fastBlob.style.left = `calc(${x}px / 6)`;
+
+        slowBlob.style.top = `calc(${y}px / 24)`;
+        slowBlob.style.left = `calc(${x}px / 24)`;
+      });
+    }
   }, []);
 
   return (
     <div className={styles.overlay}>
+      <div className={styles.cursor} />
       <img
         src="/assets/background/yellow-blob.webp"
         className={classes(styles.blob1, styles.blob)}
