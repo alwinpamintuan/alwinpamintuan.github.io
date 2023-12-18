fce1bd2330b7ee3569b2fbaca877499ad293a141
Alwin Pamintuan <alwinpamintuan@gmail.com>
2023-12-18 16:51:53 +0800

    Update blob

--- src/common/components/background/Background.tsx
+++ src/common/components/background/Background.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { classes } from "@utils/index";
 import styles from "./Background.module.css";
-import { cursorTo } from "readline";
 
 type Props = {
   section: String;
@@ -13,23 +12,15 @@ export function Background({ section }: Props) {
     const hasMobileUserAgent = /Mobi/i.test(window.navigator.userAgent);
     setIsMobile(hasMobileUserAgent);
 
-    const cursor = document.querySelector(`.${styles.cursor}`) as HTMLElement;
-    const slowBlob = document.querySelector(`.${styles.blob1}`) as HTMLElement;
-    const fastBlob = document.querySelector(`.${styles.blob2}`) as HTMLElement;
+    const bigBlob = document.querySelector(`.${styles.bigBlob}`) as HTMLElement;
 
-    if (!hasMobileUserAgent && cursor && slowBlob && fastBlob) {
+    if (bigBlob) {
       document.addEventListener("mousemove", (e) => {
         let x = e.pageX;
         let y = e.pageY;
 
-        cursor.style.top = `calc(${y}px)`;
-        cursor.style.left = `calc(${x}px)`;
-
-        fastBlob.style.top = `calc(${y}px / 6)`;
-        fastBlob.style.left = `calc(${x}px / 6)`;
-
-        slowBlob.style.top = `calc(${y}px / 24)`;
-        slowBlob.style.left = `calc(${x}px / 24)`;
+        bigBlob.style.top = `calc(${y}px / 12 + 66vh)`;
+        bigBlob.style.left = `calc(${x}px / 24)`;
       });
     }
   };
@@ -43,10 +34,9 @@ export function Background({ section }: Props) {
     case "hero":
       background = (
         <div className={styles.overlay}>
-          <div className={styles.cursor} />
           <img
             src="/assets/background/yellow-blob.webp"
-            className={classes(styles.blob1, styles.blob)}
+            className={classes(styles.bigBlob, styles.blob)}
           ></img>
         </div>
       );
